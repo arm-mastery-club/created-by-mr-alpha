b1f06746101dee24b2d917f97ebc12f0e95988e7
arm-mastery-club <mr.alpha.arm@gmail.com>
2025-12-16 18:08:04 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/created-by-mr-alpha/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
